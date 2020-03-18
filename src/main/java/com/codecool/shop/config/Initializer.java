75e1572c5154739072988790f111975a15d630e4
PeterBorok <peter.borok@gmail.com>
2020-03-18 13:21:47 +0100

    Add new book products and cover pictures

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -37,5 +37,11 @@ public class Initializer implements ServletContextListener {
         productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
         productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
         productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("The diving bell and the butterfly", 49.9f, "USD", "", tablet, amazon));
+        productDataStore.add(new Product("The troop", 49.9f, "USD", "", tablet, amazon));
+        productDataStore.add(new Product("Player piano", 49.9f, "USD", "", tablet, amazon));
+        productDataStore.add(new Product("Homeland", 49.9f, "USD", "", tablet, amazon));
+        productDataStore.add(new Product("A scanner darkly", 49.9f, "USD", "", tablet, amazon));
     }
+
 }
