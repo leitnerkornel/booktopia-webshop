009788efb14b58865e94394c4dba6c9efed62435
PeterBorok <peter.borok@gmail.com>
2020-04-02 14:14:51 +0200

    create a trial test

--- src/main/test/com/codecool/shop/model/ProductTest.java
+++ src/main/test/com/codecool/shop/model/ProductTest.java
@@ -1,7 +1,13 @@
 package com.codecool.shop.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
 
+    @Test
+    void test1() {
+        assertEquals(1, 1);
+    }
 }
