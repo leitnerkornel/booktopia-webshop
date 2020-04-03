c9da50f8618675ae36589738ff9f6a39ac8f6b95
PeterBorok <peter.borok@gmail.com>
2020-04-03 08:43:49 +0200

    add connection.properties file

--- src/main/java/com/codecool/shop/config/SQLConnection.java
+++ src/main/java/com/codecool/shop/config/SQLConnection.java
@@ -1,7 +1,9 @@
 package com.codecool.shop.config;
 
 
+import java.io.FileReader;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
@@ -29,6 +31,44 @@ public class SQLConnection {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        /*try
+        {
+            Properties props = new Properties();
+
+            String dbSettingsPropertyFile = "src/main/resources/connection.properties";
+            FileReader fReader = new FileReader(dbSettingsPropertyFile);
+
+            props.load(fReader);
+
+            String dbDriverClass = props.getProperty("db.driver.class");
+
+            String dbConnUrl = props.getProperty("url");
+
+            String dbUserName = props.getProperty("user");
+
+            String dbPassword = props.getProperty("password");
+
+            if(!"".equals(dbDriverClass) && !"".equals(dbConnUrl))
+            {
+                Class.forName(dbDriverClass);
+
+                Connection dbConn = DriverManager.getConnection(dbConnUrl, dbUserName, dbPassword);
+
+                DatabaseMetaData dbMetaData = dbConn.getMetaData();
+
+                String dbName = dbMetaData.getDatabaseProductName();
+
+                String dbVersion = dbMetaData.getDatabaseProductVersion();
+
+                System.out.println("Database Name : " + dbName);
+
+                System.out.println("Database Version : " + dbVersion);
+            }
+
+        }catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }*/
     }
 
     public static Connection getDb() {
