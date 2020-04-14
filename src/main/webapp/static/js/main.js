fbff1d6bc5a3b54e158c341ce390df4e9a74e38e
Kornél <leitner.kornel@gmail.com>
2020-04-14 18:44:55 +0200

    Filter functions removed from main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,40 +1,9 @@
 import { shoppingCart } from "./shopping_cart.js";
-
-let filterByOption = function () {
-    let filterRecommenderButtons = document.querySelectorAll('.dropdown-item');
-    let cards = document.querySelectorAll(".card");
-    for (let button of filterRecommenderButtons) {
-        let recommenderName = button.innerHTML;
-        button.addEventListener("click", function () {
-            selectByOrderOption(recommenderName, cards)
-        });
-    }
-};
-
-let selectByOrderOption = function (optionName, cards) {
-    for (let card of cards) {
-        if (isValidOrderOption(optionName)) {
-            if (!card.classList.contains(optionName)) {
-                //card.classList.add("not-selected");
-                card.parentElement.classList.add("not-selected");
-            } else {
-                card.parentElement.classList.remove("not-selected");
-            }
-        } else {
-            card.parentElement.classList.remove("not-selected");
-        }
-    }
-};
-
-let isValidOrderOption = function (orderOption) {
-    const recommenders = new Set(['Gabor', 'Kornel', 'Peti']);
-    const genres = new Set(["Thriller", "Satire", "Fantasy", "Childrens"]);
-    return recommenders.has(orderOption) || genres.has(orderOption);
-};
+import { filters } from "./filters.js";
 
 const main = function () {
     shoppingCart.shoppingCartMain();
-    filterByOption();
+    filters.filterByOption();
 };
 
 main();
