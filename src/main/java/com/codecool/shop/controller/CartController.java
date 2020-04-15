ea46f44aa94e4fae6911ceb3aab926fa7232a582
Kornél <leitner.kornel@gmail.com>
2020-04-15 11:30:02 +0200

    Create class for manage data from JSON

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -3,6 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductDaoJdbc;
+import com.sun.tools.sjavac.server.RequestHandler;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -11,19 +12,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 
-@WebServlet(urlPatterns = {"/shopping-cart"})
+@WebServlet(urlPatterns = {"/shoppingCart"})
 public class CartController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+
+        String cartJson = req.getReader().lines().collect(Collectors.joining());
+        System.out.println(cartJson);
+        String param = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+        RequestIdContainer requestIdContainer = new Gson().fromJson(param, RequestIdContainer.class);
+        int id = Integer.parseInt(requestIdContainer.getId());
+        String author = requestIdContainer.getAuthor();
+        String title = requestIdContainer.getTitle();
+        String price = requestIdContainer.getPrice();
+        System.out.println(id + author + title + price);
+        String protocol = req.getProtocol();
+        System.out.println(req.getHeaderNames());
+        System.out.println(req.getContentType());
+
+        System.out.println(protocol);
         //ProductDao productDataStoreDB = ProductDaoJdbc.getInstance();
         System.out.println(resp.toString());
-        String cartJson = req.getReader().lines().collect(Collectors.joining());
+
         Gson gson = new Gson();
         System.out.println(cartJson);
 
@@ -36,7 +54,7 @@ public class CartController extends HttpServlet {
         // params.put("category", productCategoryDataStore.find(1));
         // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         // context.setVariables(params);
-        engine.process("product/index.html", context, resp.getWriter());
+        //engine.process("product/index.html", context, resp.getWriter());
     }
 }
 
