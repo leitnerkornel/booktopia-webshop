82d3ec9c50fd56bf972772362e8643c013ef2933
Kornél <leitner.kornel@gmail.com>
2020-04-15 11:30:32 +0200

    New class for data

---
+++ src/main/java/com/codecool/shop/controller/RequestIdContainer.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.controller;
+
+public class RequestIdContainer {
+    public String id;
+    public String author;
+    public String title;
+    public String price;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+}
