658e90b86043c4aca2b81bc9e5abc1fb8d4027df
PeterBorok <peter.borok@gmail.com>
2020-04-17 07:11:46 +0200

    add checkForm method

--- src/main/webapp/static/js/checkout.js
+++ src/main/webapp/static/js/checkout.js
@@ -1,14 +1,32 @@
+function checkForm(form) {
+    let inputs = form.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+            if(inputs[i].value === ""){
+                alert("Please fill all required fields");
+                return false;
+            }
+
+    }
+    return true;
+}
+
 function backToMain() {
     let backButton = document.querySelector(".backBtn");
     backButton.addEventListener("click", function () {
-        window.location = "/";
+            window.location = "/";
+
+
     })
 }
 
 function toPayment() {
     let payButton = document.querySelector(".payBtn");
     payButton.addEventListener("click", function () {
-        window.location = "/payment";
+        let form = document.querySelector("form");
+        if (checkForm(form) === true){
+            window.location = "/payment";
+        } else {
+        }
     })
 }
 
