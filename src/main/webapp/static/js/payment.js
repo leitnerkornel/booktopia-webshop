27c13db95058e50686d67e0c19feee9174a78fab
PeterBorok <peter.borok@gmail.com>
2020-04-17 07:18:37 +0200

    extend checkForm method to payment

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -1,3 +1,15 @@
+function checkForm(form) {
+    let inputs = form.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        if(inputs[i].value === ""){
+            alert("Please fill all required fields");
+            return false;
+        }
+
+    }
+    return true;
+}
+
 function backToShippingINfo() {
     let backButton = document.querySelector(".backBtn");
     backButton.addEventListener("click", function () {
@@ -8,7 +20,11 @@ function backToShippingINfo() {
 function finalizeOrder() {
     let payButton = document.querySelector(".payBtn");
     payButton.addEventListener("click", function () {
-        alert("Guadalupei Szent Szűzanya áldja meg rendelésed!");
+        let form = document.querySelector("form");
+        if (checkForm(form) === true){
+            alert("Guadalupei Szent Szűzanya áldja meg rendelésed!");
+        } else {
+        }
     })
 }
 
